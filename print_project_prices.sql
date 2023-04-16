3d27e65b858fe0344dc267e3cf2d864bf3f21ab6
Roman Tkachenko <110996538+RomanTkachenko1996@users.noreply.github.com>
2023-04-16 21:54:53 +0300

    Update print_project_prices.sql
    edited order by

--- print_project_prices.sql
+++ print_project_prices.sql
@@ -3,4 +3,5 @@ SUM(TIMESTAMPDIFF(MONTH, p.start_date, p.finish_date) * w.salary) AS overall_pri
 FROM project_worker pw
 LEFT JOIN worker w ON pw.worker_id = w.id
 LEFT JOIN project p ON pw.project_id = p.id
-GROUP BY pw.project_id;
+GROUP BY pw.project_id
+ORDER BY SUM(TIMESTAMPDIFF(MONTH, p.start_date, p.finish_date) * w.salary) DESC;
